7b23c28bbc7852a2c3f3c61c9b4d0dc8d15526fa
vyshnavipulusu <15pa1a05e1@vishnu.edu.in>
2019-01-29 14:19:14 +0530

    Api for getadvertisementDetails

--- src/main/java/com/advertisement/advertisement/Controller/AdvertisementController.java
+++ src/main/java/com/advertisement/advertisement/Controller/AdvertisementController.java
@@ -7,8 +7,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class AdvertisementController {
@@ -23,17 +22,50 @@ public class AdvertisementController {
     }
     @GetMapping("/getAdvertisement")
     public AdvertisementDTO select(@RequestParam String category){
+        System.out.println(category);
         AdvertisementDTO advertisementDTO=new AdvertisementDTO();
         Advertisement advertisement=advertisementService.getDetails(category);
         BeanUtils.copyProperties(advertisement, advertisementDTO);
         return advertisementDTO;
     }
+    public HashMap<String,Double> sortByValue(HashMap<String,Double> m){
+        List<Map.Entry<String, Double> > list =
+                new ArrayList<Map.Entry<String,Double>>(m.entrySet());
+        // Sort the list
+        Collections.sort(list, new Comparator<Map.Entry<String, Double> >() {
+            public int compare(Map.Entry<String, Double> o1,
+                               Map.Entry<String, Double> o2)
+            {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+        });
+        int count=0;
+        // put data from sorted list to hashmap
+        HashMap<String, Double> temp = new LinkedHashMap<String, Double>();
+        for (Map.Entry<String, Double> aa : list) {
+            if(count<3) {
+                count++;
+                temp.put(aa.getKey(), aa.getValue());
+            }
+        }
+        return temp;
+    }
     @GetMapping("/getDetails")
     public AdvertisementDTO selectDetails(){
-
-        String category="";
+        HashMap<String,Double> map=new HashMap<>();
+        map.put("movies",5.4);
+        map.put("sports",7.21);
+        map.put("space",3.3);
+        map.put("fashion",3.6);
+        HashMap<String,Double> tmp=sortByValue(map);
+        //System.out.println(tmp);
+        int num=1;
+        List<String> keys = new ArrayList<String>(tmp.keySet());
+        Random rand = new Random();
+        String key = keys.get(rand.nextInt(keys.size()));
+        System.out.println(key);
         AdvertisementDTO advertisementDTO=new AdvertisementDTO();
-        Advertisement advertisement=advertisementService.getDetails(category);
+        Advertisement advertisement=advertisementService.getDetails(key);
         BeanUtils.copyProperties(advertisement, advertisementDTO);
         return advertisementDTO;
     }
